dea815fda152b80daa101dd8f959591a05ebb9ca
Ingo Fiedler <ingo.fiedler@zf.com>
2022-07-02 22:34:15 +0200

    Milestone 3 completed.

---
+++ milestone_3/binary_node3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace binary_node3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var root = new BinaryNode<string>("Root");
+        var a = new BinaryNode<string>("A");
+        var b = new BinaryNode<string>("B");
+        var c = new BinaryNode<string>("C");
+        var d = new BinaryNode<string>("D");
+        var e = new BinaryNode<string>("E");
+        var f = new BinaryNode<string>("F");
+
+        root.AddLeft(a);
+        root.AddRight(b);
+        a.AddLeft(c);
+        a.AddRight(d);
+        b.AddRight(e);
+        e.AddLeft(f);
+
+        // Find some values.
+        FindValue(root, "Root");
+        FindValue(root, "E");
+        FindValue(root, "F");
+        FindValue(root, "Q");
+
+        // Find F in the B subtree
+        FindValue(b, "F");
+    }
+
+    private static void FindValue(BinaryNode<string> startNode, string target)
+    {
+        var foundNode = startNode.FindNode(target);
+        if (foundNode != null)
+            Console.WriteLine($"Found {foundNode.Value}");
+        else
+            Console.WriteLine($"Value {target} not found");
+    }
+}
---
+++ milestone_3/nary_node3/Program.cs
@@ -0,0 +1,50 @@
+﻿using nary_node2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var root = new NaryNode<string>("Root");
+        var a = new NaryNode<string>("A");
+        var b = new NaryNode<string>("B");
+        var c = new NaryNode<string>("C");
+        var d = new NaryNode<string>("D");
+        var e = new NaryNode<string>("E");
+        var f = new NaryNode<string>("F");
+        var g = new NaryNode<string>("G");
+        var h = new NaryNode<string>("H");
+        var i = new NaryNode<string>("I");
+
+        root.AddChild(a);
+        root.AddChild(b);
+        root.AddChild(c);
+
+        a.AddChild(d);
+        a.AddChild(e);
+
+        d.AddChild(g);
+
+        c.AddChild(f);
+
+        f.AddChild(h);
+        f.AddChild(i);
+
+        // Find some values.
+        FindValue(root, "Root");
+        FindValue(root, "E");
+        FindValue(root, "F");
+        FindValue(root, "Q");
+
+        // Find F in the C subtree
+        FindValue(c, "F");
+    }
+
+    private static void FindValue(NaryNode<string> startNode, string target)
+    {
+        var foundNode = startNode.FindNode(target);
+        if (foundNode != null)
+            Console.WriteLine($"Found {foundNode.Value}");
+        else
+            Console.WriteLine($"Value {target} not found");
+    }
+}
