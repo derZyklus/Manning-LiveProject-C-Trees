f0033a5db924d1b7192732ebaec0b25239e10674
Ingo Fiedler <ingo.fiedler@zf.com>
2022-07-04 20:44:24 +0200

    Now the node drawing of the nary tree is correct.

---
+++ milestone_5/nary_node5/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nary_node5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Build a test tree.
+            // A
+            //         |
+            //     +---+---+
+            // B   C   D
+            //     |       |
+            //    +-+      +
+            // E F      G
+            //    |        |
+            //    +      +-+-+
+            // H      I J K
+            var nodeA = new NaryNode<string>("A");
+            var nodeB = new NaryNode<string>("B");
+            var nodeC = new NaryNode<string>("C");
+            var nodeD = new NaryNode<string>("D");
+            var nodeE = new NaryNode<string>("E");
+            var nodeF = new NaryNode<string>("F");
+            var nodeG = new NaryNode<string>("G");
+            var nodeH = new NaryNode<string>("H");
+            var nodeI = new NaryNode<string>("I");
+            var nodeJ = new NaryNode<string>("J");
+            var nodeK = new NaryNode<string>("K");
+
+            nodeA.AddChild(nodeB);
+            nodeA.AddChild(nodeC);
+            nodeA.AddChild(nodeD);
+            nodeB.AddChild(nodeE);
+            nodeB.AddChild(nodeF);
+            nodeD.AddChild(nodeG);
+            nodeE.AddChild(nodeH);
+            nodeG.AddChild(nodeI);
+            nodeG.AddChild(nodeJ);
+            nodeG.AddChild(nodeK);
+
+            // Draw the tree.
+            nodeA.ArrangeAndDrawSubtree(MainCanvas, 10, 10);
+        }
+
+        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            var oha = e.GetPosition(this.MainCanvas);
+
+            LabelSize.Content = $"x: {oha.X} y:{oha.Y}";
+        }
+    }
+}
--- milestone_5/nary_node5/NaryNode.cs
+++ milestone_5/nary_node5/NaryNode.cs
@@ -1,24 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
-namespace nary_node4;
+namespace nary_node5;
 
 public class NaryNode<T>
 {
-    #region Properties
-
-    public IList<NaryNode<T>> Children { get; }
-
-    public T Value { get; set; }
-
-    #endregion
-
     private const string Whitespace = "  ";
+
+    private const double NODE_RADIUS = 10.0;
+    private const double X_SPACING = 20.0;
+    private const double Y_SPACING = 30.0;
 
     public NaryNode(T value)
     {
         Value = value;
         Children = new List<NaryNode<T>>();
     }
+
+    public Rect SubtreeBounds { get; private set; }
+    public Point Center { get; private set; }
 
     public void AddChild(NaryNode<T> child)
     {
@@ -90,5 +94,111 @@ public class NaryNode<T>
         foreach (var child in Children) sb.Append($"{child.ToString(spaces + Whitespace)}");
 
         return sb.ToString();
+    }
+
+    #region Properties
+
+    public IList<NaryNode<T>> Children { get; }
+
+    public T Value { get; set; }
+
+    #endregion
+
+    public void ArrangeAndDrawSubtree(Canvas canvas, double xmin, double ymin)
+    {
+        ArrangeSubtree(xmin, xmin);
+        DrawSubtreeLinks(canvas);
+        DrawSubtreeNodes(canvas);
+    }
+
+    private void ArrangeSubtree(double xmin, double ymin)
+    {
+        if (Value.Equals("G"))
+        {
+            var aaa = "Hallo";
+        }
+
+        if (Children.Count == 0)
+        {
+            Center = new Point(xmin + NODE_RADIUS, ymin + NODE_RADIUS);
+            SubtreeBounds = new Rect(xmin, ymin, NODE_RADIUS * 2, NODE_RADIUS * 2);
+            return;
+        }
+
+        // Set child_xmin and child_ymin to the
+        // start position for child subtrees.
+        double child_xmin = xmin;
+        double child_ymin = ymin + 2 * NODE_RADIUS + Y_SPACING;
+
+        // Set ymax equal to the largest Y position used.
+        double ymax = ymin + 2 * NODE_RADIUS;
+
+        double maxSubtreeHight = 0;
+        // Position the child subtrees.
+        foreach (NaryNode<T> child in Children)
+        {
+            // Position this child subtree.
+            child.ArrangeSubtree(child_xmin, child_ymin);
+
+            // Update child_xmin to allow room for the subtree
+            // and space between the subtrees.
+            child_xmin += X_SPACING;
+
+            child_xmin += child.SubtreeBounds.Width;
+            var bbb = child.SubtreeBounds;
+            // Update the subtree bottom ymax.
+            ymax = child.SubtreeBounds.Bottom > ymax ? child.SubtreeBounds.Bottom : ymax;
+        }
+
+
+
+        if (Value.Equals("D"))
+        {
+            var aaa = "Hallo";
+        }
+
+        double xmax = child_xmin - X_SPACING;
+        SubtreeBounds = new Rect(xmin, ymin, xmax-xmin, ymax-ymin);
+        Center = new Point(xmin + ((xmax - xmin)/2), ymin + NODE_RADIUS);
+    }
+
+    private void DrawSubtreeLinks(Canvas canvas)
+    {
+        if (Children.Count == 1)
+        {
+            // ...
+            Children[0].DrawSubtreeLinks(canvas);
+        }
+        else if(Children.Count > 0)
+        {
+
+            foreach (var child in Children)
+            {
+                child.DrawSubtreeLinks(canvas);
+            }
+        }
+
+        //if (!Value.Equals("G") && !Value.Equals("A"))
+        //{
+        //    var aaa = "Hallo";
+        //    return;
+        //}
+        canvas.DrawRectangle(SubtreeBounds, null, Brushes.Red, 1);
+    }
+
+    private void DrawSubtreeNodes(Canvas canvas)
+    {
+        var nodeTopLeftPoint = new Point(Center.X - NODE_RADIUS, Center.Y - NODE_RADIUS);
+        var nodeSize = new Size(NODE_RADIUS * 2, NODE_RADIUS * 2);
+        var nodeRect = new Rect(nodeTopLeftPoint, nodeSize);
+
+        canvas.DrawEllipse(nodeRect, Brushes.White, Brushes.Black, 1);
+        canvas.DrawLabel(nodeRect, Value, Brushes.Transparent, Brushes.Red, HorizontalAlignment.Center,
+            VerticalAlignment.Center, 10, 0);
+
+        foreach (var child in Children)
+        {
+            child.DrawSubtreeNodes(canvas);
+        }
     }
 }
