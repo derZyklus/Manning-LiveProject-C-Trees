4d0cfd8dba98362a196ccc46c7ae0a584693bdae
Ingo Fiedler <ingo.fiedler@zf.com>
2022-07-06 07:19:27 +0200

    Create empty wpf application with the basic controls.

---
+++ milestone_7/sorted_binary_node1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sorted_binary_node1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RemoveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void FindButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ResetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
